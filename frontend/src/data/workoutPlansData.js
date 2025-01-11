ddd415a362240768081b01b97a49d0d579b50d78
Kivikk <kontakt@kuvik.de>
2025-01-11 22:09:20 +0100

    Update: Adapted frontend for German categories

--- frontend/src/data/workoutPlansData.js
+++ frontend/src/data/workoutPlansData.js
@@ -3,26 +3,26 @@ export const workoutPlans = [
     id: '5-min-routine',
     title: "5 Minuten Routine",
     exercises: [
-      { image: "RueckenRolle.svg", category: ["Core"] },
-      { image: "DehnungKindhaltung.svg", category: ["Back"] },
-      { image: "Vorbeuger.svg", category: ["Back"] }
+      { image: "RueckenRolle.svg", category: ["Wirbelsäule"] },
+      { image: "DehnungKindhaltung.svg", category: ["Rücken"] },
+      { image: "Vorbeuger.svg", category: ["Rücken"] }
     ]
   },
   {
     id: 'after-work',
     title: "After-Work Routine",
     exercises: [
-      { image: "BandSchulter.svg", category: ["Shoulder"] },
-      { image: "NackenRolle.svg", category: ["Back"] }
+      { image: "BandSchulter.svg", category: ["Schultern"] },
+      { image: "NackenRolle.svg", category: ["Rücken"] }
     ]
   },
   {
     id: 'complete',
     title: "Komplett Routine",
     exercises: [
-      { image: "Beinheben.svg", category: ["Legs"] },
-      { image: "HueftHebung.svg", category: ["Hip"] },
-      { image: "WadenHueftDehnung.svg", category: ["Legs", "Hip"] }
+      { image: "Beinheben.svg", category: ["Beine"] },
+      { image: "HueftHebung.svg", category: ["Hüfte"] },
+      { image: "WadenHueftDehnung.svg", category: ["Beine", "Hüfte"] }
     ]
   }
 ];
